142db2f226592fe421b7047ccebdff10f32a088d
Devin Spears <69259220+SpearsDevin@users.noreply.github.com>
2021-09-22 13:15:23 -0500

    Add files via upload

---
+++ Projects/ASCII Art/src/ArtWork.java
@@ -0,0 +1,16 @@
+/*
+ * Devin Spears
+ * 9/22/2021
+ * This program is an ASCII art project
+ * 
+ */
+class ArtWork {
+
+	public static void main(String[] args) {
+
+		String row1 = "     *****\n   *********\n ***   *   ***\n ***  ***  ***\n *************\n  **       **\n    *******\n       ** \n       ** \n  ************\n       **\n       **\n       **\n       **\n      ****\n     **  **\n    **    **\n   **      **";
+		
+		System.out.print(row1);
+
+	}
+}
